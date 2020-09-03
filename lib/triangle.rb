ce684cdf1ebb071b1d285c046e722001a8e95f1b
Richard Kurwitz <kurwitz3@aol.com>
2020-09-03 00:24:11 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ def initialize(side_1,side_2,side_3)
 end 
 
 def kind
-  if side_1 + side_2 + side_3 == 0 || (@side_1 + @side_2) <= @side_3 || (@side_2 + @side_3 <= @side_1 || @side_1 + @side_3 <= @side_2
+  if (side_1 + side_2 + side_3) == 0 || (@side_1 + @side_2) <= @side_3 || (@side_2 + @side_3) <= @side_1 || (@side_1 + @side_3) <= @side_2
    begin
         raise TriangleError
       puts error.message
