1b75385e705ebe1e54ab487f507a48dec122b465
Richard Kurwitz <kurwitz3@aol.com>
2020-09-03 00:06:01 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,25 @@
 class Triangle
-  # write code here
+ attr_accessor :side_1, :side_2, :side_3
+
+def initialize(side_1,side_2,side_3)
+ @side_1 = side_1
+ @side_2 = side_2
+ @side_3 = side_3
+end 
+
+def kind
+  if side_1 + side_2 + side_3 == 0 || @side_1 + @side_2 <= @side_3 || @side_2 + @side_3 <= @side_1 || @side_1 + @side_3 <= @side_2
+   begin
+        raise TriangleError
+end 
+else if side_1 == side_2 && side_1 == side_3
+  self.kind == equilateral
+  else if side_1 == side_2 || side_1 == side_3 || side_2 == side_3
+  self.kind == isosceles
+  else
+  self.kind == scalene
+   end
+  end
 end
+
+ 
