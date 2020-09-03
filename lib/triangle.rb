6c744c59f62244e06cf667a4a2b122de6be63636
Richard Kurwitz <kurwitz3@aol.com>
2020-09-03 00:31:20 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,20 +9,22 @@ end
 
 def kind
   if (side_1 + side_2 + side_3) == 0 || (@side_1 + @side_2) <= @side_3 || (@side_2 + @side_3) <= @side_1 || (@side_1 + @side_3) <= @side_2
-   begin
+  
+      begin
         raise TriangleError
-      puts error.message
-end 
-  else if side_1 == side_2 && side_1 == side_3
+        puts error.message
+        end 
+   
+   else if side_1 == side_2 && side_1 == side_3
     self.kind = :equilateral
   else if side_1 == side_2 || side_1 == side_3 || side_2 == side_3
     self.kind = :isosceles
   else
     self.kind = :scalene
-      end
-    end
-  end
-end 
+       end
+     end
+   end
+ end 
 end
  class TriangleError < StandardError
     def message 
