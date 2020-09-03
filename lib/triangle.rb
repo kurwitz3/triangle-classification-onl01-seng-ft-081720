80c3524372a0b275f2c6105017ccac121b48983e
Richard Kurwitz <kurwitz3@aol.com>
2020-09-03 00:24:57 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -14,11 +14,11 @@ def kind
       puts error.message
 end 
   else if side_1 == side_2 && side_1 == side_3
-    self.kind = equilateral
+    self.kind = :equilateral
   else if side_1 == side_2 || side_1 == side_3 || side_2 == side_3
-    self.kind = isosceles
+    self.kind = :isosceles
   else
-    self.kind = scalene
+    self.kind = :scalene
       end
     end
   end
